832f1786e82a7b5827401b07a610774ca5de2970
Aymen Jdily <87182808+Aymenjdily@users.noreply.github.com>
2022-12-24 01:58:24 +0100

    last one

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import { Features, Footer, Goals, Header, Motivation, Services } from '../components'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <div className='snap-y min-h-[100vh] snap-mandatory scrollbar scrollbar-thumb-gray-900 scrollbar-track-gray-100'>
+      <Head>
+      <title>Impact</title>
+      </Head>
       <div className='snap-start'>
         <Header/>
       </div>
